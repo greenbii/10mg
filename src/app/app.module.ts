4ca201e1f4c5c45ae51b6e91f92f875a96850476
Abbeydevs <abbey.developer@gmail.com>
2022-12-05 07:41:55 -0800

    bug fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeroComponent } from './components/hero/hero.component';
-import { LandingComponent } from './components/landing/landing.component';
-import { TrustedclientsComponent } from './components/trustedclients/trustedclients.component';
-import { BtnCompComponent } from './components/btn-comp/btn-comp.component';
-import { BecomeSupplierComponent } from './components/become-supplier/become-supplier.component';
+import { NavbarComponent } from './components/landingComponent/navbar/navbar.component';
+import { FooterComponent } from './components/landingComponent/footer/footer.component';
+import { HeroComponent } from './components/landingComponent/hero/hero.component';
+import { LandingComponent } from './components/landingComponent/landing/landing.component';
+import { TrustedclientsComponent } from './components/landingComponent/trustedclients/trustedclients.component';
+import { BtnCompComponent } from './components/landingComponent/btn-comp/btn-comp.component';
+import { BecomeSupplierComponent } from './components/landingComponent/become-supplier/become-supplier.component';
 
 @NgModule({
   declarations: [
