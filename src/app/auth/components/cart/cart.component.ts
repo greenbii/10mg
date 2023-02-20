7b648989705c433b9ea40425c9eca9db62fa7e9f
Aniwura Tijani <teedon@gmail.com>
2023-02-20 12:34:11 +0100

    payment integration

--- src/app/auth/components/cart/cart.component.ts
+++ src/app/auth/components/cart/cart.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AppService } from 'src/app/services/app.service';
 
+declare var PaystackPop: any;
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -18,11 +20,15 @@ export class CartComponent implements OnInit {
 
   total_weight: number = 0;
 
+  is_operation_in_progress: boolean = false;
+
 
   constructor(private appService: AppService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     const dt: any = this.route.snapshot.data;
+
+    
 
 
 
@@ -135,6 +141,64 @@ export class CartComponent implements OnInit {
     }
   }
 
+
+  openPayStackPop() {
+
+    var handler = PaystackPop.setup({
+      //key: "pk_test_ba0a38979fe2a07c6af7f98881d87a0923ca9fd0",
+      key: 'pk_test_4def4a4f871bc1e954aa89fae1da4d48909716c4', // Replace with your public key
   
+      email: this.appService.current_user.email,
+  
+      amount: (this.getSubtotal() + this.getDeliveryFee()) * 100, // the amount value is multiplied by 100 to convert to the lowest currency unit
+  
+      currency: 'NGN', // Use GHS for Ghana Cedis or USD for US Dollars
+
+      label: this.appService.current_user.displayName,
+
+      //metadata: {business_id: this.appService.current_business_details.business.id},
+  
+      //ref: Math.random().toString().split(".")[1], // Replace with a reference you generated
+  
+      callback: async (response: any)=> {
+  
+        //this happens after the payment is completed successfully
+        this.is_operation_in_progress = true;
+  
+        var reference = response.reference;
+
+        //this.appService.showNotification("Completing debit/credit card addition, please wait...");
+        //this.is_adding_in_progress = true;
+  
+        //alert('Payment complete! Reference: ' + reference);
+        const token = await this.appService.getCurrentUserToken(); //().getIdToken().getJwtToken();
+        const resp = await this.appService.initiateHttpRequest('/post', '/order' ,{reference: reference}, token).toPromise();
+        if(resp?.status === true) {
+          //notify the user that the payment method has been added
+          //or rather display the payment methods for the user
+          //this.payment_methods.push(resp.data);
+          alert(resp?.message);
+        }
+        else {
+          //this.appService.showErrorNotification(resp.msg);
+          alert(resp?.message)
+        }
+
+        //this.is_adding_in_progress = false;
+  
+        // Make an AJAX call to your server with the reference to verify the transaction
+        this.is_operation_in_progress = false;
+      },
+  
+      onClose: ()=> {
+  
+        alert('Transaction was not completed, window closed.');
+  
+      },
+  
+    });
+  
+    handler.openIframe();
+  }
 
 }
