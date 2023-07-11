ae96588af61a39eb7cc346916a0527a9a06e13cb
teedon <teedon@gmail.com>
2023-07-11 10:56:29 +0100

    update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  //apiURL: "https://api.greenbii.com/10mg"
-  apiURL: "https://orca-app-lj2z7.ondigitalocean.app/10mg",
+  apiURL: "https://api.10mg.co.uk/10mg",
+  //apiURL: "https://orca-app-lj2z7.ondigitalocean.app/10mg",
   fincra_key: "pk_NjQ1MjBlZDc2NzcwNTMyMGMzNmQ5ZTNhOjoxOTg0Mjk="
 };
