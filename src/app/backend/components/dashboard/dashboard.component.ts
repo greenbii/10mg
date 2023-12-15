f1ce72f61c941de20e6b0d9e221a10afbf20930a
teedon <teedon@gmail.com>
2023-12-15 15:49:25 +0100

    update: update chart data

--- src/app/backend/components/dashboard/dashboard.component.ts
+++ src/app/backend/components/dashboard/dashboard.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ChartComponent } from 'angular2-chartjs';
 import { AppService } from 'src/app/services/app.service';
+
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
+
+  //@ViewChild(BaseChartDirective) private _chart;
+  @ViewChild("myChart") myChart! : ChartComponent
+
+  chart_category = 'revenue';
+  chart_period = 'monthly';
 
   headings = [
     {key: "loan", label: "Loans"},
@@ -18,6 +27,7 @@ export class DashboardComponent implements OnInit {
 
   selected_filter_item: string = 'revenue';
   selected_filter_period: string = 'monthly';
+  is_loading_chart_data: boolean = false;
 
   supplier = {
     income: 0,
@@ -34,7 +44,7 @@ export class DashboardComponent implements OnInit {
 
   summary: any = null;
 
-  type = 'bar';
+  type = 'line';
   data = {
     labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
     datasets: [
@@ -45,18 +55,8 @@ export class DashboardComponent implements OnInit {
       // },
       {
         label: "Revenue",
-        data: [50000, 100000, 20000, 115000, 27890, 56721, 67900, 121234, 67890,43215,678543,22000]   ,
+        data: [0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0]   ,
         borderColor: 'rgb(0, 0, 255)',
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.7)',
-          'rgba(255, 159, 64, 0.8)',
-          'rgba(255, 205, 86, 0.5)',
-          'rgba(75, 192, 192, 0.4)',
-          'rgba(54, 162, 235, 0.6)',
-          'rgba(153, 102, 255, 0.7)',
-          'rgba(201, 203, 207, 0.2)'
-        ],
-        minBarLength: 20,
         borderWidth: 2
       }
     ]
@@ -66,6 +66,10 @@ export class DashboardComponent implements OnInit {
     maintainAspectRatio: false
   };
   constructor(private route: ActivatedRoute, private appService: AppService) { }
+  ngAfterViewInit(): void {
+    //throw new Error('Method not implemented.');
+    //console.log(this.myChart.data);
+  }
 
   ngOnInit(): void {
     const dt: any = this.route.snapshot.data;
@@ -81,6 +85,7 @@ export class DashboardComponent implements OnInit {
       this.orders.shipped = this.summary.total_orders.filter((f: any)=> f.status.toLowerCase() === "shipped").length;
 
     }
+
   }
 
   async smart() {
@@ -91,6 +96,34 @@ export class DashboardComponent implements OnInit {
     }
     catch(e) {
       console.log(e);
+    }
+  }
+
+  async getChartData() {
+    try {
+    //this should enable the user to get the chart data
+      this.is_loading_chart_data = true;
+      const t = await this.appService.getCurrentUserToken();
+      const rs = await this.appService.initiateHttpRequest('get', '/admin/analytics/filter?category='+this.chart_category+"&period="+this.chart_period, undefined, t).toPromise();
+      this.is_loading_chart_data = false;
+      if(rs?.status === true) {
+        //handle the display of the record here
+        const heading = (this.chart_period === 'monthly') ? ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"] : ['2021', '2022', '2023'];
+        if(this.myChart) {
+          this.myChart.data.labels = heading;
+          this.myChart.data.datasets[0].label = this.chart_category+"("+this.chart_period+")";
+          this.myChart.data.datasets[0].data = rs.data;
+          this.myChart.chart.update();
+        }
+
+      }
+      else {
+        throw rs?.message;
+      }
+    }
+    catch(ee) {
+      console.log(ee);
+      this.is_loading_chart_data = false;
     }
   }
 
